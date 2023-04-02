0488f8486fbc2081860f8ed9867026ce84b31cba
Max Bonfim <max@nowa.link>
2023-04-02 02:10:28 -0300

    feat: expose MemoryDB

--- gomdb.go
+++ gomdb.go
@@ -10,23 +10,22 @@ import (
 
 type document map[string]interface{}
 
-type memoryDB struct {
+type MemoryDB struct {
 	collections map[string][]document
 	Name        string
 	mu          sync.RWMutex
 }
 
-func NewMemoryDB(dbName string) *memoryDB {
+func NewMemoryDB(dbName string) *MemoryDB {
 	collections := make(map[string][]document)
 
-	return &memoryDB{
+	return &MemoryDB{
 		collections: collections,
 		Name:        dbName,
 	}
 }
 
-func (mdb *memoryDB) InsertOnCollection(collectionName string, doc map[string]interface{}) {
-
+func (mdb *MemoryDB) InsertOnCollection(collectionName string, doc map[string]interface{}) {
 	doc = mdb.injectId(doc)
 	doc = mdb.injectFields(doc)
 	doc = mdb.injectInSync(doc)
@@ -35,7 +34,7 @@ func (mdb *memoryDB) InsertOnCollection(collectionName string, doc map[string]in
 	mdb.collections[collectionName] = append(mdb.collections[collectionName], doc)
 }
 
-func (mdb *memoryDB) getCollection(collectionName string) []document {
+func (mdb *MemoryDB) getCollection(collectionName string) []document {
 	mdb.mu.RLock()
 	defer mdb.mu.RUnlock()
 	if ok := mdb.collections[collectionName]; ok == nil {
@@ -44,12 +43,12 @@ func (mdb *memoryDB) getCollection(collectionName string) []document {
 	return mdb.collections[collectionName]
 }
 
-func (mdb *memoryDB) injectId(doc map[string]interface{}) map[string]interface{} {
+func (mdb *MemoryDB) injectId(doc map[string]interface{}) map[string]interface{} {
 	doc["_id"] = uuid.New().String()
 	return doc
 }
 
-func (mdb *memoryDB) injectFields(doc map[string]interface{}) map[string]interface{} {
+func (mdb *MemoryDB) injectFields(doc map[string]interface{}) map[string]interface{} {
 	fields := make([]string, 0)
 
 	for k := range doc {
@@ -60,7 +59,7 @@ func (mdb *memoryDB) injectFields(doc map[string]interface{}) map[string]interfa
 	return doc
 }
 
-func (mdb *memoryDB) injectInSync(doc map[string]interface{}) map[string]interface{} {
+func (mdb *MemoryDB) injectInSync(doc map[string]interface{}) map[string]interface{} {
 	if doc["_in_sync"] != nil {
 		return doc
 	}
@@ -69,7 +68,7 @@ func (mdb *memoryDB) injectInSync(doc map[string]interface{}) map[string]interfa
 	return doc
 }
 
-func (mdb *memoryDB) FindOnCollection(collectionName string, filter map[string]interface{}) []document {
+func (mdb *MemoryDB) FindOnCollection(collectionName string, filter map[string]interface{}) []document {
 	mdb.mu.RLock()
 	defer mdb.mu.RUnlock()
 
@@ -85,8 +84,7 @@ func (mdb *memoryDB) FindOnCollection(collectionName string, filter map[string]i
 	return filteredDocs
 }
 
-func (mdb *memoryDB) UpdateOnCollection(collectionName string, filter, update map[string]interface{}) int {
-
+func (mdb *MemoryDB) UpdateOnCollection(collectionName string, filter, update map[string]interface{}) int {
 	collection := mdb.getCollection(collectionName)
 	updatedCount := 0
 
@@ -101,7 +99,7 @@ func (mdb *memoryDB) UpdateOnCollection(collectionName string, filter, update ma
 	return updatedCount
 }
 
-func (mdb *memoryDB) makeChange(doc *document, update map[string]interface{}) {
+func (mdb *MemoryDB) makeChange(doc *document, update map[string]interface{}) {
 	mdb.mu.Lock()
 	defer mdb.mu.Unlock()
 	for key, value := range update {
@@ -112,11 +110,11 @@ func (mdb *memoryDB) makeChange(doc *document, update map[string]interface{}) {
 	}
 }
 
-func (mdb *memoryDB) keyCanBeChanged(key string) bool {
+func (mdb *MemoryDB) keyCanBeChanged(key string) bool {
 	return key != "_id" && key != "_fields" && key != "_in_sync"
 }
 
-func (mdb *memoryDB) parseDocument(doc document, filter map[string]interface{}) bool {
+func (mdb *MemoryDB) parseDocument(doc document, filter map[string]interface{}) bool {
 	matchs := 0
 
 	for filterKey, filterValue := range filter {
